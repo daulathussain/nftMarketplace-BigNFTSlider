26ab429fa452f571a905371cc0e9c076cbc8f8df
daulathussain <daulathussain92@gmail.com>
2022-08-25 22:33:43 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/index.module.css";
+import {
+  HeroSection,
+  Service,
+  BigNFTSilder,
+} from "../components/componentsindex";
+
+const Home = () => {
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+      <BigNFTSilder />
+    </div>
+  );
+};
+
+export default Home;
